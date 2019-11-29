9d5cab46c5c3b02148293c68a874c0c8a4025096
Gudlaug Agnes Kristjánsdóttir <gak16@hi.is>
2019-11-29 01:58:15 +0000

    local storage til að vafri muni klarada fyrirlestra, óklárað

--- src/lib/locStorage.js
+++ src/lib/locStorage.js
@@ -1,2 +1,7 @@
-export function saveLecture();
+export function saveLecture(){
+  let slug = item.slug;
+  window.localStorage.setItem(slug, 'finished');
+  console.log('Saved to local storage.');
+  console.log(slug);
+}
 
