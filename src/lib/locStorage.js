c5371d1a83ee0f831312127fe0b900da38a21015
Gudlaug Agnes Kristjánsdóttir <gak16@hi.is>
2019-11-29 02:01:20 +0000

    lagaði syntax villur samkvæmt stylelintinu og eslintinu

--- src/lib/locStorage.js
+++ src/lib/locStorage.js
@@ -1,7 +1,6 @@
-export function saveLecture(){
-  let slug = item.slug;
+export function saveLecture() {
+  const slug = item.slug;
   window.localStorage.setItem(slug, 'finished');
   console.log('Saved to local storage.');
   console.log(slug);
 }
-
