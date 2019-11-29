652d4961ff35ef1a20171e4149c7c180da1225a8
Gudlaug Agnes Kristjánsdóttir <gak16@hi.is>
2019-11-29 01:39:01 +0000

    sækja fyrirlestra efnið, grunnur, þarf að betrumbæta

--- src/lib/list.js
+++ src/lib/list.js
@@ -3,9 +3,23 @@ import { empty } from './helpers';
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.url = '/lectures.json';
+  }
+
+  // sækja fyrirlestra efnið
+  loadLectures() {
+    return fetch(this.url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Ekki var hægt að sækja fyrirlestra.');
+        }
+        return res.json();
+      });
   }
 
   load() {
     empty(this.container);
+    this.loadLectures()
+      .then(data => this.renderData(data));
   }
 }
