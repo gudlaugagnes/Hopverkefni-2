e0cf3fe481d2c9397ddac5be3c23c7097f8c8916
Gudlaug Agnes Kristjánsdóttir <gak16@hi.is>
2019-11-29 01:14:04 +0000

    elements í helpers skránni , þarf að klára að tengja við rétta klasa í samræmi við css og html

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,3 +3,21 @@ export function empty(element) {
     element.removeChild(element.firstChild);
   }
 }
+
+export function createElem(elem, text) {
+  const el = document.createElement(elem);
+  if (text) {
+    el.appendChild(document.createTextNode(text));
+  }
+  return el;
+}
+
+export function cardsAppear(value) {
+  const new_Val = value.toLowerCase();
+
+  for (const card of document.querySelectorAll('.card')) {
+    if (card.classList.contains(new_Val)) {
+      card.className = `card ${new_Val}`;
+    }
+  }
+}
